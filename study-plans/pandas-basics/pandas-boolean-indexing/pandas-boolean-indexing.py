477069c9f5c648fc07f510e6fbb00d0045408256
Karan Sehgal <145564588+Karansehgal0611@users.noreply.github.com>
2026-04-09 00:25:04 +0530

    Add solution for pandas-boolean-indexing

---
+++ study-plans/pandas-basics/pandas-boolean-indexing/pandas-boolean-indexing.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def boolean_filter(data, column, threshold):
+    """
+    Returns: dict with 'filtered_data' (dict) and 'count' (int)
+    """
+    df = pd.DataFrame(data)
+    filtered_data = df[df[column] > threshold]
+
+    return {
+        "filtered_data" : filtered_data.to_dict('list'),
+        "count" : list(filtered_data.shape)[0]
+    }
+    
+    pass
