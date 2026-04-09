834f2988c17aff32cd58c75a1b2e5487351b7e2d
Karan Sehgal <145564588+Karansehgal0611@users.noreply.github.com>
2026-04-09 19:08:54 +0530

    Update solution for pandas-handle-missing

--- study-plans/pandas-basics/pandas-handle-missing/pandas-handle-missing.py
+++ study-plans/pandas-basics/pandas-handle-missing/pandas-handle-missing.py
@@ -8,7 +8,7 @@ def handle_missing(data, fill_value):
     null_countsdf = {k: int(v) for k, v in df.isnull().sum().items()}
     df_cleaned = df.fillna(fill_value)
     return {
-        "null_counts" : null_countsdf,
+        "null_counts" : df.isna().sum().to_dict(),
         "cleaned_data" : df_cleaned.to_dict('list')
     }
     
